2d83a0acbb6e76b507d61983290b4377cf9ff9ed
Prabish <prabishdangi2002@gmail.com>
2023-10-06 22:04:54 +0545

    miror jobs styling changes

--- src/Components/Jobs.jsx
+++ src/Components/Jobs.jsx
@@ -78,14 +78,50 @@ const Data = [
     time: "2 days ago",
     company: "Atkans",
   },
+  {
+    id: 1,
+    title: "Dev-Ops",
+    jobType: "Full-Time",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi facilis nulla necessitatibus?",
+    location: "Dubai",
+    time: "2 days ago",
+    company: "Atkans",
+  },
+  {
+    id: 1,
+    title: "Dev-Ops",
+    jobType: "Full-Time",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi facilis nulla necessitatibus?",
+    location: "Dubai",
+    time: "2 days ago",
+    company: "Atkans",
+  },
+  {
+    id: 1,
+    title: "Dev-Ops",
+    jobType: "Full-Time",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi facilis nulla necessitatibus?",
+    location: "Dubai",
+    time: "2 days ago",
+    company: "Atkans",
+  },
+  {
+    id: 1,
+    title: "Dev-Ops",
+    jobType: "Full-Time",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi facilis nulla necessitatibus?",
+    location: "Dubai",
+    time: "2 days ago",
+    company: "Atkans",
+  },
   // Add more job objects to the Data array if needed
 ];
 
 const Jobs = () => {
   return (
-    <div className="jobsType-Container flex flex-wrap gap-10 justify-evenly items-center rounded-[10px] py-10 mt-10">
+    <div className="jobsType-Container flex flex-wrap gap-50 justify-evenly items-center rounded-[10px] py-10 mt-10">
       {Data.map(({ id, title, jobType, description, location, time, company }) => (
-        <div key={id} className="jobType-container-child bg-slate-50 group group/item w-[250px] p-[20px] hover:bg-blue-500 hover:text-white shadow-lg shadow-greyIsh-400/700 hover:shadow-lg rounded-[10px]">
+        <div key={id} className="jobType-container-child mb-10 p-5 w-[30%] bg-slate-100 group group/item  hover:bg-blue-500 hover:text-white shadow-lg shadow-greyIsh-400/700 hover:shadow-lg rounded-[10px]">
           <div className="jobtype-container">
             <div className="jobType-container-child-content">
               <h3 className="jobcard text-center text-xl font-semibold text-gray-700 mb-4">{title}</h3>
@@ -105,7 +141,7 @@ const Jobs = () => {
               </div>
             </div>
           </div>
-          <button className="applyButton mt-3 rounded-[10px] bg-sky-600 w-[100%] text-white font-semibold text-[14px] p-[10px] "> Apply Now</button>
+          <button className="applyButton mt-3 rounded-[10px] bg-sky-600 w-[100%] text-white font-semibold text-[14px] p-[10px] ">Apply Now</button>
         </div>
       ))}
     </div>
