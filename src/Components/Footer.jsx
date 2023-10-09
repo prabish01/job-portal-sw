e8be155286c510becad9878f2ad59fcec5d5c81c
Prabish <prabishdangi2002@gmail.com>
2023-10-09 08:45:56 +0545

    added footer template for adjustment

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,18 @@
 import React from "react";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer__parent w-[100%] bg-orange-200 rounded-[20px] p-10">
+      <div className="footer__content flex justify-around pb-11">
+        <h3>Footer 1</h3>
+
+        <h3>Footer 2</h3>
+        <h3>Footer 3</h3>
+        <h3>Footer 4</h3>
+      </div>
+      <p className="opacity-[0.5] text-slate-800">© 2021 Copyright: Speedwings Human Resource</p>
+    </div>
+  );
 };
 
 export default Footer;
